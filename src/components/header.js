40bbd6f0de3241c34f97f695f9355c8ab27870da
Feli <srgatoiscool@gmail.com>
2020-03-31 23:48:29 +0000

    added material-ui

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Button } from '@material-ui/core'
 
 class Header extends Component {
-    render(){
+    render() {
         return (
             <div>
-                <h1>Platillos Tipicos</h1>
+                <h1>Platillo</h1>
+                <Button variant="contained" color="primary">
+                    Hello World
+    </Button>
             </div>
         );
     }
